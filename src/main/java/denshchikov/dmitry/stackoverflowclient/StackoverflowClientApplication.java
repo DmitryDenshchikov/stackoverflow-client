b24aff39ec098b752b9cbc90b03117a82ab83166
Dmitry Denshchikov <mr.dd33@yandex.ru>
2023-05-24 23:07:40 +0300

    Initial commit

---
+++ src/main/java/denshchikov/dmitry/stackoverflowclient/StackoverflowClientApplication.java
@@ -0,0 +1,13 @@
+package denshchikov.dmitry.stackoverflowclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StackoverflowClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StackoverflowClientApplication.class, args);
+	}
+
+}
---
+++ src/test/java/denshchikov/dmitry/stackoverflowclient/StackoverflowClientApplicationTests.java
@@ -0,0 +1,13 @@
+package denshchikov.dmitry.stackoverflowclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StackoverflowClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
